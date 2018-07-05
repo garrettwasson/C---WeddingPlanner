67f93f4a9868e8428a2a4b4f24486cad279d0db4
Garrett <garrettgucci@gmail.com>
2018-07-05 00:44:07 -0700

    initial commit; can't get the RSVP's to work properly; I believe it is an issue in the Database and there being an issue in which data is passing to the Database, as the conditionals, AND database info is displaying properly if manually injected; also can NOT delete a wedding if there is data inserted in to the RSVP table

---
+++ Controllers/WeddingController.cs
@@ -0,0 +1,104 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using WeddingPlanner.Models;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+
+namespace WeddingPlanner.Controllers
+{
+    public class WeddingController : Controller
+    {
+        private Context _context;
+        private User ActiveUser 
+        {
+            get{ return _context.users.Where(u => u.user_id == HttpContext.Session.GetInt32("id")).FirstOrDefault();}
+        }
+        public WeddingController(Context context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            if(ActiveUser == null)
+                return RedirectToAction("Index", "Home");
+            Dashboard dashData = new Dashboard
+            {
+                Weddings = _context.weddings.Include(w => w.Guests).ToList(),
+                User = ActiveUser
+            };
+        return View(dashData);
+        }
+        public IActionResult Create()
+        {
+            if(ActiveUser == null)
+                return RedirectToAction("Index", "Home");
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(ViewWedding newWedding)
+        {
+            if(ActiveUser == null)
+                return RedirectToAction("Index", "Home");
+            if(ModelState.IsValid)
+            {
+                Wedding wedding = new Wedding
+                {
+                    bride_name = newWedding.WedderOne,
+                    groom_name = newWedding.WedderTwo,
+                    date = newWedding.Date,
+                    address = newWedding.Address,
+                    user_id = ActiveUser.user_id
+                };
+                _context.weddings.Add(wedding);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(newWedding);
+        }
+        [HttpPost]
+        public IActionResult RSVP(int id)
+        {
+            if(ActiveUser == null)
+                return RedirectToAction("Index", "Home");
+            RSVP rsvps = new RSVP
+            {
+                user_id = ActiveUser.user_id,
+                wedding_id = id
+            };
+            _context.rsvps.Add(rsvps);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        [HttpPost]
+        public IActionResult UnRSVP(int id)
+        {
+            if(ActiveUser == null)
+                return RedirectToAction("Index", "Home");
+            RSVP toDelete = _context.rsvps.Where(r => r.wedding_id == id)
+                                          .Where(r => r.user_id == ActiveUser.user_id)
+                                          .SingleOrDefault();
+            _context.rsvps.Remove(toDelete);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public IActionResult Show(int id)
+        {
+            if(HttpContext.Session.GetInt32("id") == null)
+               return RedirectToAction("Index");
+            return View(_context.weddings.Include(w => w.Guests).ThenInclude(g => g.Guest).Where(w => w.wedding_id == id).SingleOrDefault());
+        }
+        [HttpGet("weddings/{id}/delete")]
+        public IActionResult Delete(int id)
+        {
+            Wedding retrievedWedding = _context.weddings.Where(w => w.wedding_id == id).SingleOrDefault();
+            _context.weddings.Remove(retrievedWedding);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        
+    }
+}
